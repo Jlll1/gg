f3c00a3ab8ffb16381e7051b12257badf6433bab
Jlll1 <71319302+Jlll1@users.noreply.github.com>
2022-03-30 22:42:46 +0200

    Update to go 1.18 and use generics (#4)

--- utils/array.go
+++ utils/array.go
@@ -2,14 +2,27 @@ package utils
 
 import "fmt"
 
-func TryGet(arr []string, index int) (string, error) {
+func TryGet[T any](arr []T, index int) (T, error) {
+	var result T
 	if index < 0 {
-		return "", fmt.Errorf("Index %d cannot be smaller than 0", index)
+		return result, fmt.Errorf("Index %d cannot be smaller than 0", index)
 	}
 
 	if index >= len(arr) {
-		return "", fmt.Errorf("Tried to access out of range index %d", index)
+		return result, fmt.Errorf("Tried to access out of range index %d", index)
 	}
 
 	return arr[index], nil
+}
+
+func Filter[T any](arr []T, predicate func(item T) bool) []T {
+	count := 0
+	for _, item := range arr {
+		if predicate(item) {
+			arr[count] = item
+			count++
+		}
+	}
+
+	return arr[:count]
 }
